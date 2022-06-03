cd955e539905738c90623254efe61b80554a79a8
Chunpeng Huo <huocp@me.com>
2022-06-03 10:20:07 +1000

    test: add some test coverage

--- test/id-utils.spec.js
+++ test/id-utils.spec.js
@@ -334,5 +334,6 @@ test('mapId returns mapped id', t => {
   t.equal(mapId('text!../src/foo/bar.html', paths), 'text!foo/bar.html');
   t.equal(mapId('el!../src/foo/bar.html', paths), '@some/name-space/el!foo/bar.html');
   t.equal(mapId('vs/editor/editor.main.js', paths), 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.20.0/min/vs/editor/editor.main.js');
+  t.equal(mapId('vs', paths), 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.20.0/min/vs');
   t.end();
 });
